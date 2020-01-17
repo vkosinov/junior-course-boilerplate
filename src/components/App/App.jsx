dbf82badfbb3f4838232c6e27873088e0e87b19a
Viktor Kosinov <vkosinov@ya.ru>
2020-01-17 14:34:18 +0300

    topik State and uncontrolled controls

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,53 @@ import React from 'react';
 import { Title } from '../Title';
 import { Container } from '../Container';
 import { ProductList } from '../ProductList';
+import { Filter } from '../Filter';
+
+import { getPrice } from '../../utils';
+
+import s from './styles.module.css';
 
 import products from '../../products';
 
-export const App = () => {
-  return (
-    <Container>
-      <Title>Список товаров</Title>
+const priceArr = products.map(item => getPrice(item.price));
 
-      <ProductList items={products} />
-    </Container>
-  );
-};
+const maxPrice = priceArr.sort((a, b) => b - a)[0];
+
+const minPrice = priceArr.sort((a, b) => a - b)[0];
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      minPrice: minPrice,
+      maxPrice: maxPrice,
+    };
+  }
+
+  handleFilter = stateFilter => {
+    this.setState(stateFilter);
+  };
+
+  render() {
+    return (
+      <Container>
+        <Title>Список товаров</Title>
+
+        <div className={s.main}>
+          <div className={s.aside}>
+            <Filter
+              maxPrice={this.state.maxPrice}
+              minPrice={this.state.minPrice}
+              handleFilter={this.handleFilter}
+            />
+          </div>
+
+          <div className={s.article}>
+            <ProductList items={products} maxPrice={this.state.maxPrice} minPrice={this.state.minPrice} />
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ProductItem from 'csssr-school-product-card';
+import { LogRender } from '../LogRender';
+
+import { getPrice } from '../../utils';
 
 import s from './styles.module.css';
 
@@ -9,26 +12,38 @@ const ratingComponent = ({ isFilled }) => {
   return <div className={isFilled ? 'starFill' : ''} />;
 };
 
-export const ProductList = ({ items }) => {
-  return (
-    <div className={s.wrap}>
-      {items.map(item => (
-        <div className={s.wrapItem} key={item.id}>
-          <ProductItem
-            isInStock={item.isInStock}
-            img={item.img}
-            title={item.name}
-            price={item.price}
-            subPriceContent={item.subPriceContent}
-            maxRating={5}
-            rating={item.rating}
-            ratingComponent={ratingComponent}
-          />
-        </div>
-      ))}
-    </div>
-  );
+const getSelectedProduct = (items, maxPrice, minPrice) => {
+  return items.filter(item => getPrice(item.price) <= minPrice && getPrice(item.price) >= maxPrice);
 };
+
+export class ProductList extends LogRender {
+  render() {
+    if (this.props.maxPrice >= 0 && this.props.minPrice >= 0) {
+      this.products = getSelectedProduct(this.props.items, this.props.minPrice, this.props.maxPrice);
+    } else {
+      this.products = this.props.items;
+    }
+
+    return (
+      <div className={s.wrap}>
+        {this.products.map(item => (
+          <div className={s.wrapItem} key={item.id}>
+            <ProductItem
+              isInStock={item.isInStock}
+              img={item.img}
+              title={item.name}
+              price={item.price}
+              subPriceContent={item.subPriceContent}
+              maxRating={5}
+              rating={item.rating}
+              ratingComponent={ratingComponent}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 ProductList.propTypes = {
   items: PropTypes.arrayOf(
