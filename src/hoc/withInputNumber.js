c9a3daf97bbe3c867e2fb059589b8d767c1d34ce
Viktor Kosinov <vkosinov@ya.ru>
2020-01-22 11:56:34 +0300

    add withInputNumber

---
+++ src/hoc/withInputNumber.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { toInt } from 'csssr-school-utils';
+
+export default OriginalComponent =>
+  class withInputNumber extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        value: this.props.value,
+      };
+    }
+
+    handleChange = events => {
+      const newValue = toInt(events.target.value);
+
+      if (newValue !== this.state.value) {
+        this.setState({
+          value: newValue,
+        });
+
+        if (this.props.onChange) {
+          this.props.onChange(newValue);
+        }
+      }
+    };
+
+    render() {
+      return <OriginalComponent {...this.props} value={this.state.value} onChange={this.handleChange} />;
+    }
+  };
